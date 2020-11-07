6287216aa0d7acd8b2cef196cbc84eafb6469d3a
RK <ra10101@yahoo.com>
2020-11-07 11:55:05 +0530

    First commit

---
+++ config/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+//mongoose.connect(db)
+const connectDB = async() => {
+    try{
+
+        await mongoose.connect(db, {
+             useNewUrlParser: true , 
+             useUnifiedTopology: true , 
+             useCreateIndex: true,
+             useFindandModufy: false
+            } );
+
+        console.log('MongoDB Connected...');
+
+    }catch(err) {
+
+        console.error(err.message);
+        //Exit process with failure
+        process.exit(1);
+
+    }
+}
+
+module.exports = connectDB;
